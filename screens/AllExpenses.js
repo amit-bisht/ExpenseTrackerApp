088cb62b63bcf2deff8fcc2c5a15bb57326c8fc5
amit-bisht <bishtakb69@gmail.com>
2022-09-25 17:20:45 +0530

    second commit

---
+++ screens/AllExpenses.js
@@ -0,0 +1,9 @@
+import {View,Text} from 'react-native'
+function AllExpenses(){
+    return(
+        <View>
+            <Text>All Expense</Text>
+        </View>
+    )
+}
+export default AllExpenses
---
+++ screens/RecentExpense.js
@@ -0,0 +1,9 @@
+import {View,Text} from 'react-native'
+function RecentExpense(){
+    return(
+        <View>
+            <Text>Recent Expense</Text>
+        </View>
+    )
+}
+export default RecentExpense
